2c01a833478d8dc549df0ab60b204b170318991e
Andrea Angelini <andrea-angelini@outlook.it>
2019-09-05 21:44:52 +0200

    First

---
+++ build/CMakeFiles/Test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test.dir/src/main.cpp.obj"
+  "Debug/Test.exe"
+  "Debug/Test.exe.manifest"
+  "Debug/Test.lib"
+  "Debug/Test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+ // https://www.40tude.fr/blog/compile-cpp-code-with-vscode-cmake-nmake/
+
+int main() 
+{
+    std::cout << "Hello World\n";
+    int bob = 3;
+    getchar();
+}
