f1e1859aa52ffef405b1aa95b8dfe44305f0c8e4
Andrea Angelini <andrea-angelini@outlook.it>
2019-09-05 23:52:45 +0200

    added sdl2. Does not work atm

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
-#include <iostream>
-#include "Objects\baseobject.h"
+#include "SDL.h"
 
+#undef main
 
  // https://www.40tude.fr/blog/compile-cpp-code-with-vscode-cmake-nmake/
 
 int main() 
 {
-    std::cout << "Hello World\n";
+  SDL_Init(SDL_INIT_VIDEO);
 
-    base_object* obj = new base_object();
+  SDL_Window *window = SDL_CreateWindow(
+    "SDL2Test",
+    SDL_WINDOWPOS_UNDEFINED,
+    SDL_WINDOWPOS_UNDEFINED,
+    640,
+    480,
+    0
+  );
 
-    obj->print();
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
 
-    delete obj;
+  SDL_Delay(3000);
 
-    getchar();
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
 }
