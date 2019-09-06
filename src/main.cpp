0c3052a8571ae4fd3fc0b0126dd1f7bd42fec1f6
Andrea Angelini <andrea-angelini@outlook.it>
2019-09-06 16:07:26 +0200

    tab fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "SDL.h"
 #ifdef IS_WINDOWS
-  #include <windows.h>
+#include <windows.h>
 #endif
 #include "GL/gl.h"
 #include <stdio.h>
@@ -8,51 +8,49 @@
 
 #undef main
 
- // https://www.40tude.fr/blog/compile-cpp-code-with-vscode-cmake-nmake/
+// https://www.40tude.fr/blog/compile-cpp-code-with-vscode-cmake-nmake/
 
-int main() 
+int main()
 {
-    SDL_Init(SDL_INIT_VIDEO);
+	SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *window = SDL_CreateWindow(
-      "My first cpp-cmake application",
-      SDL_WINDOWPOS_CENTERED,
-      SDL_WINDOWPOS_CENTERED,
-      640,
-      480,
-      SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
-    );
+	SDL_Window *window = SDL_CreateWindow(
+		"My first cpp-cmake application",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		640,
+		480,
+		SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 
-    std::vector<int> vec;
-    vec.push_back(1);
+	std::vector<int> vec;
+	vec.push_back(1);
 
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-    SDL_GLContext context = SDL_GL_CreateContext(window);
+	SDL_GLContext context = SDL_GL_CreateContext(window);
 
-    bool running = true;
+	bool running = true;
 
-    printf("CULO");
+	printf("My first workable cmake-cpp app.");
 
-    while (running)
-    {
-        SDL_Event event;
+	while (running)
+	{
+		SDL_Event event;
 
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT)
-                running = false;
-        }
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+				running = false;
+		}
 
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 
-       
-        SDL_GL_SwapWindow(window);
-    }
-   
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+		SDL_GL_SwapWindow(window);
+	}
 
-    return 0;
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
